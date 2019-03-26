0bdb991c59f4e43bb692e95fb8a1d656cce9d098
Avery-Dante <drksknddv@Avery-Dantes-MacBook-Air.local>
2019-03-26 12:50:49 -0400

    creating  circular progress bar

--- lib/bottom_controls.dart
+++ lib/bottom_controls.dart
@@ -1,6 +1,3 @@
-
-import 'dart:math';
-
 import 'package:flutter/material.dart';
 import 'package:music_player/theme.dart';
 
@@ -188,16 +185,3 @@ class NextButton extends StatelessWidget {
   }
 }
 
-class CircleClipper extends CustomClipper<Rect> {
-   @override 
-  Rect getClip(Size size) {
-    return new Rect.fromCircle(
-      center: new Offset(size.width / 2, size.height / 2),
-      radius: min(size.width, size.height) / 2,
-    );
-  }
-  @override
-  bool shouldReclip(CustomClipper<Rect> oldClipper){
-    return true;
-  }
-}
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:music_player/bottom_controls.dart';
 import 'package:music_player/songs.dart';
-
 
 void main() => runApp(MyApp());
 
@@ -17,7 +18,6 @@ class MyApp extends StatelessWidget {
         primaryColorLight: Colors.green,
         primarySwatch: Colors.yellow,
         accentColor: Colors.green,
-        
       ),
       home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
@@ -42,12 +42,10 @@ class _MyHomePageState extends State<MyHomePage> {
         elevation: 0.0,
         leading: new IconButton(
           icon: new Icon(
-            Icons.arrow_back_ios ,
+            Icons.arrow_back_ios,
           ),
           color: Colors.deepPurpleAccent,
-          onPressed: () {
-
-          },
+          onPressed: () {},
         ),
         title: Title(
           color: Colors.deepPurpleAccent,
@@ -57,25 +55,16 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       body: new Column(
         children: <Widget>[
-
           //Seek Bar Currrently just album artwork
           new Expanded(
             child: new Center(
-              child: new Container(
-                width: 125.0,
-                height: 125.0,
-
-                child: ClipOval(
-                  clipper: new CircleClipper(),
-                                  child: new Image.network(
-                    demoPlaylist.songs[0].albumArtUrl,
-                    fit: BoxFit.cover,
-                  ),
+                child: new Container(
+                    width: 125.0, 
+                    height: 125.0, 
+                    child: RadialSeekBar()
                 )
-              )
             ),
           ),
-
 
           //visualizer
           new Container(
@@ -83,15 +72,103 @@ class _MyHomePageState extends State<MyHomePage> {
             height: 125.0,
           ),
 
-
-          //Song title, artists name, player controls 
-          //A column with rows 
-          new BottomControls(),          
+          //Song title, artists name, player controls
+          //A column with rows
+          new BottomControls(),
         ],
       ),
     );
   }
 }
 
+class RadialSeekBar extends StatefulWidget {
+  final double trackWidth;
+  final Color trackColor;
+  final double progressWidth;
+  final Color progressColor;
+  final double progressPercentage;
+  final double thumbSize;
+  final Color thumbColor;
+  final double thumbPosition;
+  final Widget child;
 
- 
+  RadialSeekBar({
+    this.trackWidth = 3.0,
+    this.trackColor = Colors.indigoAccent,
+    this.progressWidth = 5.0,
+    this.progressColor = Colors.black87,
+    this.progressPercentage = 0.0,
+    this.thumbSize = 10.0,
+    this.thumbColor = Colors.black,
+    this.thumbPosition = 0.0,
+    this.child,
+  });
+  @override
+  _RadialSeekBarState createState() => new _RadialSeekBarState();
+}
+
+class _RadialSeekBarState extends State<RadialSeekBar> {
+  @override
+  Widget build(BuildContext context) {
+    return new CustomPaint(
+      painter: new RadialSeekBarPainter(),
+      child: widget.child,
+    );
+  }
+}
+
+class RadialSeekBarPainter extends CustomPainter {
+  final double trackWidth;
+  final Color trackColor;
+  final double progressWidth;
+  final Color progressColor;
+  final double progressPercentage;
+  final double thumbSize;
+  final Color thumbColor;
+  final double thumbPosition;
+
+  RadialSeekBarPainter({
+    this.trackWidth = 3.0,
+    this.trackColor = Colors.indigoAccent,
+    this.progressWidth = 5.0,
+    this.progressColor = Colors.black87,
+    this.progressPercentage = 0.0,
+    this.thumbSize = 10.0,
+    this.thumbColor = Colors.black,
+    this.thumbPosition = 0.0,
+  });
+  @override
+
+  void paint(Canvas canvas, Size size) {
+    //TODO: implement paint
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    // TODO: implement shouldRepaint
+    return true;
+  }
+}
+
+var albumArt = ClipOval(
+        clipper: new CircleClipper(),
+        child: new Image.network(
+          demoPlaylist.songs[0].albumArtUrl,
+          fit: BoxFit.cover,
+        ),
+      );
+
+class CircleClipper extends CustomClipper<Rect> {
+  @override
+  Rect getClip(Size size) {
+    return new Rect.fromCircle(
+      center: new Offset(size.width / 2, size.height / 2),
+      radius: min(size.width, size.height) / 2,
+    );
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Rect> oldClipper) {
+    return true;
+  }
+}
